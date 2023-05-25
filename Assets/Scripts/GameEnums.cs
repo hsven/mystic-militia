b5c72444b136ea78842dd0ba9293404ed2203588
Hélio Borges <helio.borges@tecnico.ulisboa.pt>
2023-05-25 18:56:22 +0100

    Added basic player and unit movement

---
+++ Assets/Scripts/GameEnums.cs
@@ -0,0 +1,16 @@
+ using UnityEngine;
+ using System.Collections;
+
+
+public static class GameEnums {
+    public enum CommandTypes
+    {
+        FOLLOW,
+        ATTACK,
+        DEFEND,
+        PATROL,
+    }
+
+
+
+}
