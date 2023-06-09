15126102ce303940b64e32acb909c313c05f2289
Hélio Borges <helio.borges@tecnico.ulisboa.pt>
2023-06-09 13:55:02 +0100

    Preliminary WFC map generation

---
+++ Assets/Scripts/Editor/ExtendedTesseraSquareTileEditor.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEditor;
+using Tessera;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(ExtendedTesseraSquareTile))]
+public class ExtendedTesseraSquareTileEditor : TesseraTileBaseEditor {
+    SerializedProperty tile;
+    SerializedProperty setOfTiles;
+    SerializedProperty blockOffset;
+    SerializedProperty possibleUnderTiles;
+
+    void OnEnable()
+    {
+        tile = serializedObject.FindProperty("tile");
+        setOfTiles = serializedObject.FindProperty("setOfTiles");
+        blockOffset = serializedObject.FindProperty("blockOffset");
+        possibleUnderTiles = serializedObject.FindProperty("possibleUnderTiles");
+    }
+
+    public override void OnInspectorGUI() {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(tile);
+        serializedObject.ApplyModifiedProperties();
+        EditorGUILayout.PropertyField(setOfTiles);
+        serializedObject.ApplyModifiedProperties();
+        EditorGUILayout.PropertyField(blockOffset);
+        serializedObject.ApplyModifiedProperties();
+        EditorGUILayout.PropertyField(possibleUnderTiles);
+        serializedObject.ApplyModifiedProperties();
+
+        EditorGUILayout.Separator();
+
+        base.OnInspectorGUI();
+        
+    }
+}
---
+++ Assets/Tessera/Lib/README.txt
@@ -0,0 +1,8 @@
+These binaries are supplied as part of the license of Tessera. They are binaries to save on Unity load time, and because it's unlikely you'll want to change them.
+
+If you need access to source code, or want to make modifications, these libraries are also available under an open source license.
+
+They can be found at:
+
+https://github.com/BorisTheBrave/DeBroglie
+https://github.com/BorisTheBrave/sylves
